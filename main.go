1fcc3c96dd25d8a3520a41ca524ac48c2f9f68fc
Alexandre Carneiro <acarneiro@e-goi.com>
2021-04-20 08:50:16 +0100

    add route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,6 +12,12 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+
+		return c.SendString(fmt.Sprintf("Hello, %s!", name))
 	})
 
 	log.Fatal(app.Listen(":3000"))
